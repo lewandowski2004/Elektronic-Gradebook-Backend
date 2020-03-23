5dc9e4367840d88d8930495299766efc53fe1bf5
lewandowski <lewandowski2004@o2.pl>
2020-03-23 16:11:47 +0100

    Adding Employee, Role services

---
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/services/EmployeeService.java
@@ -0,0 +1,102 @@
+package lewandowski.electronic_gradebook.services;
+
+
+import lewandowski.electronic_gradebook.dto.EmployeeDto;
+import lewandowski.electronic_gradebook.model.Employee;
+import lewandowski.electronic_gradebook.repository.EmployeeRepository;
+import lewandowski.electronic_gradebook.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class EmployeeService {
+
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    public void saveEmployeeDto(EmployeeDto employeeDto) {
+
+        Employee employee = Employee.builder()
+                .name(employeeDto.getName())
+                .lastName(employeeDto.getLastName())
+                .username(employeeDto.getUsername())
+                .active(employeeDto.getActive())
+                .email(employeeDto.getEmail())
+                .password(encoder.encode(employeeDto.getPassword()))
+                .roles(roleService.findByIdIn(employeeDto.getRoles()))
+                .build();
+
+        employeeRepository.save(employee);
+    }
+   /* @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void deleteUserById(UUID id){
+        User user = userRepository.findById(id);
+        userRepository.delete(user);
+    }
+
+
+    public List<UserDto> findAllUsersDto() {
+        return findAllUsersDtoList(userRepository.findAll());
+    }*/
+
+    public EmployeeDto findById(UUID id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Error: Employee is not found."));
+        return getEmployeeDto(employee);
+    }
+
+    public Employee getEmployee(EmployeeDto employeeDto) {
+        return Employee.builder()
+                .id(employeeDto.getId())
+                .name(employeeDto.getName())
+                .lastName(employeeDto.getLastName())
+                .email(employeeDto.getEmail())
+                .password(employeeDto.getPassword())
+                .active(employeeDto.getActive())
+                .roles(roleService.getRoles(employeeDto.getRoleDtos()))
+                .build();
+
+    }
+
+    public List<EmployeeDto> findAllEmployeesDtoList(List<Employee> employeeList) {
+        List<EmployeeDto> employeeDtoList = new ArrayList<>();
+        for (Employee employee : employeeList) {
+            EmployeeDto pupilDto = getEmployeeDto(employee);
+            employeeDtoList.add(pupilDto);
+        }
+        return employeeDtoList;
+    }
+
+    public List<EmployeeDto> findAllEmployeesDto() {
+        return findAllEmployeesDtoList(employeeRepository.findAll());
+    }
+
+    public EmployeeDto getEmployeeDto(Employee employee) {
+        return EmployeeDto.builder()
+                .id(employee.getId())
+                .name(employee.getName())
+                .lastName(employee.getLastName())
+                .username(employee.getUsername())
+                .email(employee.getEmail())
+                .password(employee.getPassword())
+                .roleDtos(roleService.getRolesDto(employee.getRoles()))
+                .build();
+    }
+}
---
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/services/RoleService.java
@@ -0,0 +1,65 @@
+package lewandowski.electronic_gradebook.services;
+
+
+import lewandowski.electronic_gradebook.dto.RoleDto;
+import lewandowski.electronic_gradebook.model.Role;
+import lewandowski.electronic_gradebook.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+@Service
+@Transactional
+public class RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    /*Dto method*/
+    public List<Long> getRolesIdDto(List<Role> roles) {
+        List<Long> listId = new ArrayList<>();
+        for (Role role : roles) {
+            listId.add(role.getId());
+        }
+        return listId;
+    }
+
+    public Set<Role> findByIdIn(Set<Long> listId) {
+        return roleRepository.findByIdIn(listId);
+    }
+
+    public Role getRole(RoleDto roleDto) {
+        return Role.builder()
+                .id(roleDto.getId())
+                .name(roleDto.getName())
+                .build();
+    }
+    public RoleDto getRoleDto(Role role) {
+        return RoleDto.builder()
+                .id(role.getId())
+                .name(role.getName())
+                .build();
+    }
+
+    public Set<Role> getRoles(Set<RoleDto> roleDtos) {
+        Set<Role> roles = new HashSet<>();
+        for (RoleDto roleDto : roleDtos) {
+            roles.add(getRole(roleDto));
+        }
+        return roles;
+    }
+
+    public Set<RoleDto> getRolesDto(Set<Role> roleList) {
+        Set<RoleDto> roles = new HashSet<>();
+        for (Role role : roleList) {
+            roles.add(getRoleDto(role));
+        }
+        return roles;
+    }
+}
