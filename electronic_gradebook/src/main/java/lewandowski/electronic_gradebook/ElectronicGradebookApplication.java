5b9be17a13449d26cd0ffc3b9197bc18f6df23de
lewandowski <lewandowski2004@o2.pl>
2020-03-23 14:23:29 +0100

    Creation of the basic version of the project

---
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/ElectronicGradebookApplication.java
@@ -0,0 +1,13 @@
+package lewandowski.electronic_gradebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicGradebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicGradebookApplication.class, args);
+    }
+
+}
---
+++ electronic_gradebook/src/test/java/lewandowski/electronic_gradebook/ElectronicGradebookApplicationTests.java
@@ -0,0 +1,13 @@
+package lewandowski.electronic_gradebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicGradebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
