b22f33f729b3dd7039d260ab4eda151b2d24781a
lewandowski <lewandowski2004@o2.pl>
2020-03-23 16:42:04 +0100

    Revert "Revert "er for authentication""
    This reverts commit eddc96ad7b455453feae6cf6dc7962034d659744.

---
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/controller/AuthController.java
@@ -0,0 +1,61 @@
+package lewandowski.electronic_gradebook.controller;
+
+import lewandowski.electronic_gradebook.payload.JwtResponse;
+import lewandowski.electronic_gradebook.payload.LoginRequest;
+import lewandowski.electronic_gradebook.security.JwtTokenProvider;
+import lewandowski.electronic_gradebook.security.PupilPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by rajeevkumarsingh on 02/08/17.
+ */
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtTokenProvider tokenProvider;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsernameOrEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.generateToken(authentication);
+
+        PupilPrincipal userDetails = (PupilPrincipal) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
+    }
+
+}
