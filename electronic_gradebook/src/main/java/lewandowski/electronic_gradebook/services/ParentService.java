f36ae3ec5071666c29b200e7f454a700e8e92dcf
lewandowski <lewandowski2004@o2.pl>
2020-03-23 16:13:24 +0100

    Adding Parent, Pupil services

---
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/services/ParentService.java
@@ -0,0 +1,94 @@
+package lewandowski.electronic_gradebook.services;
+
+
+import com.lewandowski.electronicgradebook.dto.ParentDto;
+import com.lewandowski.electronicgradebook.model.*;
+import com.lewandowski.electronicgradebook.repository.ParentRepository;
+import com.lewandowski.electronicgradebook.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.*;
+
+@Service
+@Transactional
+public class ParentService {
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private ParentRepository parentRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    public void saveParentDto(ParentDto parentDto) {
+        Set<Role> roles = new HashSet<>();
+        Role pupilRole = roleRepository.findByName(RoleName.ROLE_PARENT)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        roles.add(pupilRole);
+        Parent parent = Parent.builder()
+                .name(parentDto.getName())
+                .lastName(parentDto.getLastName())
+                .username(parentDto.getUsername())
+                .active(parentDto.getActive())
+                .email(parentDto.getEmail())
+                .password(encoder.encode(parentDto.getPassword()))
+                .role(pupilRole)
+                .build();
+
+        parentRepository.save(parent);
+    }
+
+    public ParentDto findById(UUID id) {
+        Parent parent = parentRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Error: Parent is not found."));
+        return getParentDto(parent);
+    }
+
+    public List<ParentDto> findAllEmployeesDto() {
+        return findAllParentsDtoList(parentRepository.findAll());
+    }
+
+    public Parent getParent(ParentDto parentDto) {
+        return Parent.builder()
+                .id(parentDto.getId())
+                .name(parentDto.getName())
+                .lastName(parentDto.getLastName())
+                .email(parentDto.getEmail())
+                .password(parentDto.getPassword())
+                .active(parentDto.getActive())
+                .role(parentDto.getRole())
+                .build();
+
+    }
+
+
+    public List<ParentDto> findAllParentsDtoList(List<Parent> parentList) {
+        List<ParentDto> parentDtoList = new ArrayList<>();
+        for (Parent parent : parentList) {
+            ParentDto parentDto = getParentDto(parent);
+            parentDtoList.add(parentDto);
+        }
+        return parentDtoList;
+    }
+
+    public ParentDto getParentDto(Parent parent) {
+        return ParentDto.builder()
+                .id(parent.getId())
+                .name(parent.getName())
+                .lastName(parent.getLastName())
+                .username(parent.getUsername())
+                .email(parent.getEmail())
+                .password(parent.getPassword())
+                .role(parent.getRole())
+                .build();
+    }
+}
---
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/services/PupilService.java
@@ -0,0 +1,94 @@
+package lewandowski.electronic_gradebook.services;
+
+
+import lewandowski.electronic_gradebook.dto.PupilDto;
+import lewandowski.electronic_gradebook.model.Pupil;
+import lewandowski.electronic_gradebook.model.Role;
+import lewandowski.electronic_gradebook.model.RoleName;
+import lewandowski.electronic_gradebook.repository.PupilRepository;
+import lewandowski.electronic_gradebook.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.*;
+
+@Service
+@Transactional
+public class PupilService {
+
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PupilRepository pupilRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    public void savePupilDto(PupilDto pupilDto) {
+        Set<Role> roles = new HashSet<>();
+        Role pupilRole = roleRepository.findByName(RoleName.ROLE_PUPIL)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        roles.add(pupilRole);
+        Pupil pupil = Pupil.builder()
+                .name(pupilDto.getName())
+                .lastName(pupilDto.getLastName())
+                .username(pupilDto.getUsername())
+                .active(pupilDto.getActive())
+                .email(pupilDto.getEmail())
+                .password(encoder.encode(pupilDto.getPassword()))
+                .role(pupilRole)
+                .build();
+
+        pupilRepository.save(pupil);
+    }
+
+    public PupilDto findById(UUID id) {
+        Pupil pupil = pupilRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Error: Pupil is not found."));
+        return getPupilDto(pupil);
+    }
+
+    public List<PupilDto> findAllPupilsDto() {
+        return findAllPupilsDtoList(pupilRepository.findAll());
+    }
+
+    public Pupil getPupil(PupilDto pupilDto) {
+        return Pupil.builder()
+                .id(pupilDto.getId())
+                .name(pupilDto.getName())
+                .lastName(pupilDto.getLastName())
+                .username(pupilDto.getUsername())
+                .email(pupilDto.getEmail())
+                .password(pupilDto.getPassword())
+                .role(pupilDto.getRole())
+                .build();
+    }
+
+    public List<PupilDto> findAllPupilsDtoList(List<Pupil> pupilList) {
+        List<PupilDto> pupilDtoList = new ArrayList<>();
+        for (Pupil pupil : pupilList) {
+            PupilDto pupilDto = getPupilDto(pupil);
+            pupilDtoList.add(pupilDto);
+        }
+        return pupilDtoList;
+    }
+
+    public PupilDto getPupilDto(Pupil pupil) {
+        return PupilDto.builder()
+                .id(pupil.getId())
+                .name(pupil.getName())
+                .lastName(pupil.getLastName())
+                .username(pupil.getUsername())
+                .email(pupil.getEmail())
+                .password(pupil.getPassword())
+                .role(pupil.getRole())
+                .build();
+    }
+}
