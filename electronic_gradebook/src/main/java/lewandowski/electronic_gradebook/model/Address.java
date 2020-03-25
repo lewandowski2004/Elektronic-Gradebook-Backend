9d559a25e112cb1ab41701da7f1c1c5f8a85e680
lewandowski <lewandowski2004@o2.pl>
2020-03-25 16:23:15 +0100

    Adding the address class

---
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/model/Address.java
@@ -0,0 +1,26 @@
+package lewandowski.electronic_gradebook.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+public class Address {
+
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "address_line_1")
+    private String addressLine1;
+
+    @Size(max = 100)
+    @Column(name = "address_line_2")
+    private String addressLine2;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "city")
+    private String city;
+
+    @NotNull
+    @Size(max = 6)
+    @Column(name = "zip_code")
+    private String zipCode;
+}
