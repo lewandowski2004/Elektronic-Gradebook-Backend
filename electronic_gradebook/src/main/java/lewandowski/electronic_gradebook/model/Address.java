c2d0940e7c740d926534adca06dc3e02d7a1d7a5
lewandowski <lewandowski2004@o2.pl>
2020-03-26 17:26:56 +0100

    Update Address class

--- electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/model/Address.java
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/model/Address.java
@@ -3,7 +3,6 @@ package lewandowski.electronic_gradebook.model;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.*;
 
 @Builder
 @Getter
@@ -12,22 +11,11 @@ import javax.validation.constraints.*;
 @Embeddable
 public class Address {
 
-    @NotNull
-    @Size(max = 100)
-    @Column(name = "address_line_1")
     private String addressLine1;
 
-    @Size(max = 100)
-    @Column(name = "address_line_2")
     private String addressLine2;
 
-    @NotNull
-    @Size(max = 100)
-    @Column(name = "city")
     private String city;
 
-    @NotNull
-    @Size(max = 6)
-    @Column(name = "zip_code")
     private String zipCode;
 }
