919ac0ba8731c2526f7237db70e799ed2ab82e06
lewandowski <lewandowski2004@o2.pl>
2020-03-26 21:37:50 +0100

    Addition Pupil data transfer object class to save

---
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/dto/PupilDtoToSave.java
@@ -0,0 +1,11 @@
+package lewandowski.electronic_gradebook.dto;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+public class PupilDtoToSave extends PupilDto {
+}
