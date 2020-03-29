c47bc4e9bf76a6162bf77711cc7f213045dadf1a
lewandowski <lewandowski2004@o2.pl>
2020-03-29 12:06:09 +0200

    Create classes for present

---
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/dto/_toPresent/EmployeeDtoToPresent.java
@@ -0,0 +1,10 @@
+package lewandowski.electronic_gradebook.dto._toPresent;
+
+import lewandowski.electronic_gradebook.dto.UserDto;
+
+import java.util.List;
+
+public class EmployeeDtoToPresent extends UserDto {
+
+    private List<String> roles;
+}
---
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/dto/_toPresent/PupilDtoToPresent.java
@@ -0,0 +1,10 @@
+package lewandowski.electronic_gradebook.dto._toPresent;
+
+import lewandowski.electronic_gradebook.dto.UserDto;
+
+import java.util.List;
+
+public class PupilDtoToPresent extends UserDto {
+
+    private List<String> roles;
+}
