f9bba7f7714728694123de1a5ca322ceabf6c0f2
lewandowski <lewandowski2004@o2.pl>
2020-03-30 18:12:50 +0200

    Updated fields in User class

--- electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/model/User.java
+++ electronic_gradebook/src/main/java/lewandowski/electronic_gradebook/model/User.java
@@ -3,8 +3,10 @@ package lewandowski.electronic_gradebook.model;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -24,14 +26,35 @@ public class User {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "second_name")
+    private String secondName;
+
     @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "pesel")
+    private String pesel;
+
+    @Column(name = "date_of_birth")
+    @Temporal(TemporalType.DATE)
+    private Date dateOfBirth;
+
+    @Column(name = "date_of_addition")
+    private Date dateOfAddition;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
 
     @Column(name = "username")
     private String username;
 
     @Column(name = "email")
     private String email;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private Gender gender;
 
     @Embedded
     private Address address;
